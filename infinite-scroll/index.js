22c5dfd97c7697fe493461a851133f6c9f40f8dc
mcshakes <efmccarthy26@gmail.com>
2020-04-20 17:15:43 -0600

    Rendering the posts on the page

--- infinite-scroll/index.js
+++ infinite-scroll/index.js
@@ -1,4 +1,4 @@
-
+const postsContainer = document.getElementById('posts-container');
 
 let limit = 6;
 let page = 1;
@@ -12,4 +12,27 @@ async function getPosts() {
     return json;
 }
 
-getPosts();
+async function showPosts() {
+    let posts = await getPosts();
+
+    posts.forEach(post => {
+        const postElem = document.createElement("div");
+
+        postElem.classList.add("post");
+
+        postElem.innerHTML = `
+            <div class="number">${post.id}</div>
+            <div class="post-info">
+                <h2 class="post-title">${post.title}</h2>
+                <p class="post-body">${post.body}</h2>
+            </div>
+        `;
+
+        postsContainer.appendChild(postElem);
+
+    })
+
+
+}
+
+showPosts();
